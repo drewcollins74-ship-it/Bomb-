6ae37ea85680cab4db4738f20aa806ff15e008b8
drewcollins74-ship-it <drewcollins74@gmail.com>
2026-07-04 11:12:15 -0400

    Initial Commit

---
+++ Bomb!/Bomb_App.swift
@@ -0,0 +1,17 @@
+//
+//  Bomb_App.swift
+//  Bomb!
+//
+//  Created by DREW COLLINS on 7/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Bomb_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
